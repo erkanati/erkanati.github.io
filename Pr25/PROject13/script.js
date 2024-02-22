98e112aa9a0bc5fa39a22be19cf52e0c3e3f6761
ERKANAT <iamerkanat@gmail.com>
2024-02-23 04:29:49 +0600

    +My version next

--- Pr25/PROject13/script.js
+++ Pr25/PROject13/script.js
@@ -6,8 +6,16 @@ const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
+const API_URL2 = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cd1cbc4c971d9aaad2ab22e8aaa5b2b5&page=2'
 //Getting movies:
 getMovies(API_URL)
+const nextPage = document.getElementById('next')
+nextPage.addEventListener('click', ()=>{
+    getMovies(API_URL2)
+})
+
+
+
 async function getMovies(url){
     const res = await fetch(url)
     const data = await res.json()
@@ -19,8 +27,8 @@ function showMovies(movies) {
     main.innerHTML = ''
 
     movies.forEach((movie) =>{
-        const { title, poster_path, vote_average, overview} = movie
-
+        const { title, poster_path, vote_average, overview, popularity, original_title, release_date} = movie
+        
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
 
@@ -28,12 +36,12 @@ function showMovies(movies) {
         
             <img src="${IMG_PATH + poster_path} alt="${title}">
             <div class="movie-info">
-                <h3>${title}</h3>
+                <h3>${getRealTitle(title, original_title)} ${getYearOnly(release_date)}<br>${parseInt(popularity)} views</h3>
                 <span class="${getClassByRate(vote_average)}">${vote_average}</span>
             </div>
             <div class="overview">
                 <h3>Overview</h3>
-                ${overview}
+                ${getOverview(overview)}
             </div>
         `
 
@@ -50,6 +58,30 @@ function getClassByRate(vote){
         return 'red'
     }
 }
+
+function getRealTitle(title, original_title){
+    if(title === original_title){
+        return title
+    } else {
+        return `${title} | ${original_title}`
+    }
+}
+
+function getYearOnly(release_date){
+    release_date = release_date.slice(0,4)
+    return release_date 
+
+
+}
+
+function getOverview(overview){
+    if (overview.length >= 400){
+        return overview.slice(0, 400)+"..."
+    } else {
+        return overview
+    }
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     
