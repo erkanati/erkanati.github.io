3add39b1b1f762bcc7669679ead35a4fef0243c1
ERKANAT <iamerkanat@gmail.com>
2024-02-24 00:40:53 +0600

    whaaaat? 130?

---
+++ Pr50/PROject17/script.js
@@ -0,0 +1,29 @@
+
+
+function dragStart(){
+    console.log('drag start')
+}
+
+function dragStart(){
+    console.log('drag start')
+}
+
+
+function dragStart(){
+    console.log('drag start')
+}
+
+
+function dragStart(){
+    console.log('drag start')
+}
+
+
+function dragStart(){
+    console.log('drag start')
+}
+
+
+function dragStart(){
+    console.log('drag start')
+}
