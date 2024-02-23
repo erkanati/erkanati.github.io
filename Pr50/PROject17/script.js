78a34de3547c655cc2262d66cf8f13d4cd19fd15
ERKANAT <iamerkanat@gmail.com>
2024-02-24 01:23:17 +0600

    PROject 17 the Drag&Drop was finally done with switch

--- Pr50/PROject17/script.js
+++ Pr50/PROject17/script.js
@@ -1,29 +1,63 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+//for(const empty of empties)
+empties.forEach((empty)=>{
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
+})
 
 
 function dragStart(){
-    console.log('drag start')
+    this.className += ' hold'
+    setTimeout(()=>this.className = 'invisible', 0)
+    
 }
 
-function dragStart(){
-    console.log('drag start')
-}
+function dragEnd(){
+    this.className='fill'
 
-
-function dragStart(){
-    console.log('drag start')
-}
-
-
-function dragStart(){
-    console.log('drag start')
 }
 
 
-function dragStart(){
-    console.log('drag start')
+function dragOver(event){
+    event.preventDefault()
+
 }
 
 
-function dragStart(){
-    console.log('drag start')
+function dragEnter(event){
+    event.preventDefault()
+    this.className += ' hovered'
+    
 }
+
+
+function dragLeave(){
+    this.className = 'empty'
+}
+
+
+function dragDrop(){
+    this.className = 'empty' 
+
+    this.append(fill) 
+}
+
+const toggle = document.querySelector('.toggle')
+
+toggle.addEventListener('click', (event)=>{
+    const html = document.querySelector('html')
+    if(html.classList.contains('dark')){
+        html.classList.remove('dark')
+        event.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+        event.target.innerHTML = 'Light mode'
+    }
+})
