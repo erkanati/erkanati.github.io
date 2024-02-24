6b292157a127a5bc4c2776008290e2f958f508b9
ERKANAT <iamerkanat@gmail.com>
2024-02-25 00:26:17 +0600

    Project2O

--- Pr50/PROjects19-22/script.js
+++ Pr50/PROjects19-22/script.js
@@ -1,3 +1,4 @@
+//pr1 I mean 19
 const panels = document.querySelectorAll('.panel')
 
 panels.forEach((panel)=>{
@@ -13,3 +14,57 @@ function removeActiveClasses(){
         panel.classList.remove('active')
     })
 }
+
+//pr2 I mean 20
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle') //Nodelist
+
+let currentActive = 1
+
+next.addEventListener('click', ()=>{
+    currentActive++
+
+    if(currentActive > circles.length){
+        currentActive = circles.length
+
+    }   
+
+    update() 
+})
+
+prev.addEventListener('click', ()=>{
+    currentActive--
+
+    if(currentActive < 1){
+        currentActive = 1
+    } 
+    
+    update()
+})
+
+function update(){
+    circles.forEach((circle, idx)=>{
+
+        if(idx < currentActive){
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = ((actives.length - 1)/ (circles.length-1)) * 100 + '%'
+
+    if(currentActive ===1){
+        prev.disabled = true
+    } else if(currentActive === circles.length){
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+}
+
