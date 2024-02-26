2839cbf7b5603c96a6483b44476b9574bd1935ac
ERKANAT <iamerkanat@gmail.com>
2024-02-27 04:14:23 +0600

    first-4PROjects were done V

--- Pr50/PROjects19-21/script.js
+++ Pr50/PROjects19-21/script.js
@@ -68,3 +68,11 @@ function update(){
     }
 }
 
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn1')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', ()=>{
+    search.classList.toggle('active')
+    input.focus()
+})
