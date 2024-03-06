805912708d1f30e7b0564e9ba5ebfe04bcfd0890
ERKANAT <iamerkanat@gmail.com>
2024-03-07 03:04:22 +0500

    Project37 circle slider was done

---
+++ Pr50/PROject37/script.js
@@ -0,0 +1,43 @@
+const left = document.getElementById('left')
+const right = document.getElementById('right')
+const imgs = document.getElementById('imgs')
+
+const img= document.querySelectorAll('#imgs img')
+
+let index = 0
+
+let interval = setInterval(run, 2000)
+
+function run(){
+    index++
+    changeImage()
+}
+
+function changeImage(){
+    if(index > img.length){
+        index = 0
+    } else if(index < 0){
+        index = img.length-1
+    }
+
+    imgs.style.transform = `translateX(${-index * 500}px`
+}
+
+function resetInterval(){
+    clearInterval(interval)
+    
+}
+
+right.addEventListener('click', ()=>{
+    index++
+
+    changeImage()
+    resetInterval()
+})
+
+
+left.addEventListener('click', ()=>{
+    index--
+    resetInterval()
+    changeImage()
+})
