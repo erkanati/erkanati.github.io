770a746d96ff8ee072ddc88fdbf0946d7e3399bb
iamerkanat <iamerkanat@gmail.com>
2024-03-16 03:37:24 +0500

    justCourseraPractise

---
+++ Pr50/PROject47/greetings.js
@@ -0,0 +1,13 @@
+export const informalGreeting = (person) =>{
+    console.log(`hello, ${person}`)
+}
+export const formalGreeting = (person) =>{
+    console.log(`Good Day, ${person}!`)
+}
+
+const greeting = () =>{
+    console.log('Howzitgoin?')
+}
+
+export default greeting
+
---
+++ Pr50/PROject47/modules.js
@@ -0,0 +1,8 @@
+//photographers need linzes for any kind of situation, so wee need JS ES6 modules
+
+//modules = stand-alone thing
+//window.function = global CAN OVERWRITE local function
+//ServerJS => CommonJS (node.js)
+
+``
+
