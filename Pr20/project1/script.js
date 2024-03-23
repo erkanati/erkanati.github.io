62f04fd5cc764d6e48a6a9a71afeb12024d1d165
iamerkanat <iamerkanat@gmail.com>
2024-03-23 05:49:10 +0500

    1 finished js

--- Pr20/project1/script.js
+++ Pr20/project1/script.js
@@ -16,36 +16,90 @@ function showSuccess(input){
     formCtrl.className = 'form-control success'
 }
 
-function checkEmail(email){
+function checkEmail(input){
     const valid = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return valid.test(String(email).toLowerCase()) 
+
+    if(valid.test(input.value.trim())){
+        showSuccess(input)
+    } else {
+        showError(input, 'Email is not valid')
+    }
+}
+
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function checkRequired(inputArray){
+    inputArray.forEach(function(input){
+        // console.log(input.value)
+
+        if(input.value.trim() === ''){
+            showError(input, ` ${getFieldName(input)} is reqired!!!`)
+        } else {
+            showSuccess(input)
+        }
+    } )
+}
+
+function checkLength(input, min, max){
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    } else if (input.value.length > max){
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+    }
+}
+
+function checkPasswords(input1, input2){
+    if(input1.value !== input2.value){
+        showError(input2, 'Passwords do not match')
+    } else{
+        showSuccess(input1)
+    }
+
 }
 
 
 form.addEventListener('submit', function(event){
     event.preventDefault()
 
-    // console.log(username.value)
-    if(username.value === ''){
-        showError(username, 'Username is required!!!')
-    } else {
-        showSuccess(username)
-    }
-
-    if(email.value === ''){
-        showError(email, 'Email is required!!!')
-    } else {
-        showSuccess(email)
-    }
-
-    if(password.value === ''){
-        showError(password, 'password is required!!!')
-    } else {
-        showSuccess(password)
-    }
-    if(password2.value === '' || password.value !== password2.value){
-        showError(password2, 'password 2 is required!!!')
-    } else {
-        showSuccess(password2)
-    }
+    checkRequired([username, email, password, password2])
+    checkLength(username, 3, 15)
+    checkLength(password, 8, 18)
+    checkEmail(email)
+    checkPasswords(password, password2)
 })
+
+
+
+//BORIIIIIIING:
+
+// console.log(username.value)
+
+
+//in EventListener
+
+// if(username.value === ''){
+    //     showError(username, 'Username is required!!!')
+    // } else {
+    //     showSuccess(username)
+    // }
+
+    // if(email.value === ''){
+    //     showError(email, 'Email is required!!!')
+    // } else if(!checkEmail(email.value)){
+    //     showError(email, 'Email is not valid')
+    // } else {
+    //     showSuccess(email)
+    // }
+
+    // if(password.value === ''){
+    //     showError(password, 'password is required!!!')
+    // } else {
+    //     showSuccess(password)
+    // }
+    // if(password2.value === '' || password.value !== password2.value){
+    //     showError(password2, 'password 2 is required!!!')
+    // } else {
+    //     showSuccess(password2)
+    // }
