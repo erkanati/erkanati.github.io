93601cd0ef40824238bedcfd9ca73cd1fd7a8f01
iamerkanat <iamerkanat@gmail.com>
2024-04-30 03:27:21 +0500

    I AM BACK!!!

---
+++ Pr20/project2/script.js
@@ -0,0 +1,30 @@
+const container = document.querySelector('.container')
+const seats = document.querySelectorAll('.row .seat:not(occupied)')
+const count = document.getElementById('count')
+const total = document.getElementById('total')
+const movieSelect = document.getElementById('movie')
+
+let ticketPrice = +movieSelect.value
+
+// console.log(typeof ticketPrice)
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected')
+    const selectedSeatsCount = selectedSeats.length
+
+    
+    count.innertext = selectedSeatsCount
+    total.innerText = selectedSeatsCount * ticketPrice
+}
+
+movieSelect.addEventListener('change', event => {
+    ticketPrice = +event.target.value
+    updateSelectedCount()
+})
+
+container.addEventListener('click', function(event){
+    if(event.target.classList.contains('seat') && !event.target.classList.contains('occupied')){
+        event.target.classList.toggle('selected')
+
+        updateSelectedCount()
+    }
+})
