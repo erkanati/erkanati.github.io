c94e9289c3e75e1e8f5d2ce828ad21ff62c3a6b0
iamerkanat <iamerkanat@gmail.com>
2025-05-30 01:00:19 +0500

    pop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Box, Container } from '@mui/material';
 import ReactGA from 'react-ga';
 import NavBar from './components/NavBar/NavBar';
@@ -8,32 +8,28 @@ import Skills from './pages/Skills/Skills';
 import Blogs from './pages/Blogs';
 import Footer from './components/Footer/Footer';
 import './App.css';
-import { HashRouter } from 'react-router-dom';
-
 import React from 'react';
-//r
+
+// Google Analytics (optional)
 if (typeof process.env.REACT_APP_TRACKING_ID !== 'undefined') {
     ReactGA.initialize(process.env.REACT_APP_TRACKING_ID);
 }
 
 function App() {
     return (
-        <HashRouter>
-            <Router>
-                <NavBar />
-                <Container sx={{ flex: 1, py: 3 }}>
-                    <Routes>
-                        <Route path="/portfolio" exact element={<Home />} />
-                        <Route path="/projects" exact element={<Projects />} />
-                        <Route path="/blogs" exact element={<Blogs />} />
-                        <Route path="/skills" exact element={<Skills />} />
-                    </Routes>
-                </Container>
-                <Footer />
-            </Router>
-        </HashRouter>
-        
+        <Router>
+            <NavBar />
+            <Container sx={{ flex: 1, py: 3 }}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/blogs" element={<Blogs />} />
+                    <Route path="/skills" element={<Skills />} />
+                </Routes>
+            </Container>
+            <Footer />
+        </Router>
     );
-};
+}
 
 export default App;
