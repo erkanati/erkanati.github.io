d20cd14d926a5fbc104a792a04b56e4c405c153e
iamerkanat <iamerkanat@gmail.com>
2025-05-30 11:09:02 +0500

    react

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-
-import { Box, Container } from '@mui/material';
-import ReactGA from 'react-ga';
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from '@mui/material';
 import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import Projects from './pages/Projects';
 import Skills from './pages/Skills/Skills';
 import Blogs from './pages/Blogs';
-import Footer from './components/Footer/Footer';
 import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 
 function App() {
   return (
-    <Router basename="/erkanati.github.io">
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/projects" element={<Projects />} />
-    <Route path="/blogs" element={<Blogs />} />
-    <Route path="/skills" element={<Skills />} />
-  </Routes>
-</Router>
+    <Router>
+      <NavBar />
+      <Container sx={{ flex: 1, py: 3 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/skills" element={<Skills />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
 export default App;
-
